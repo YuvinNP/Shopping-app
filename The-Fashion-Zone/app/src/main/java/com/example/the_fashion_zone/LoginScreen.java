caff80fa5a06be9f3dbc250ac8766f18e8105f19
YuvinNP <yuvinnimnaka1998@gmail.com>
2019-08-02 14:26:24 +0530

    minor changes

--- The-Fashion-Zone/app/src/main/java/com/example/the_fashion_zone/LoginScreen.java
+++ The-Fashion-Zone/app/src/main/java/com/example/the_fashion_zone/LoginScreen.java
@@ -2,13 +2,26 @@ package com.example.the_fashion_zone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class LoginScreen extends AppCompatActivity {
 
+    Button skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    public void skipToNext(View v){
+
+        skip = findViewById(R.id.skipbtn);
+        Intent int1 = new Intent(LoginScreen.this, LoginActivity.class);
+
+        startActivity(int1);
     }
 }
