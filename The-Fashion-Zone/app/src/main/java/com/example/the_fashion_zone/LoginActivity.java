6df282a11ae097e34151e5a1941ccd5347b65fd5
YuvinNP <yuvinnimnaka1998@gmail.com>
2019-08-02 16:31:39 +0530

    minor updates

--- The-Fashion-Zone/app/src/main/java/com/example/the_fashion_zone/LoginActivity.java
+++ The-Fashion-Zone/app/src/main/java/com/example/the_fashion_zone/LoginActivity.java
@@ -3,12 +3,22 @@ package com.example.the_fashion_zone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        getSupportActionBar().setTitle("Login");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
     }
 }
